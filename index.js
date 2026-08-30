b354c14eff1977777a18a0266c379d23a9c49439
Anonimous157 <junior.filipi2009@gmail.com>
2026-08-29 22:42:49 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let fate=document.querySelector("#data");
 let ff=document.querySelector(".hh");
 let tela_data=document.querySelector("#tela_data");
-let niver_motta=document.querySelector("#niver_motta");
+let niver_motta=document.querySelector(".niver_motta");
 let body=document.querySelector(".corpo");
+let textao=document.querySelector(".texto_principal")
+let instagram=document.querySelector(".instagram");
 let relogio=document.querySelector("#relogiom")
 let quem_tá_de_niver=0
 //a var quem_tá_de_niver é usada para setar quem é o aniversariante, e servir como uma state machine
@@ -11,6 +13,7 @@ let quem_tá_de_niver=0
 //=3 =>ARTHUR
 //=4 =>AMABILY
 //=5 =>LUCAS//
+const rosa="#ee92c7d8";
 
 fate.addEventListener("change", () => {
   if (fate.value==="2026-09-01"){
@@ -22,13 +25,35 @@ fate.addEventListener("change", () => {
   }
 })
 function motta() {
-  body.style.backgroundColor="#ee92c7d8";
-  tela_data.style.display="none";
-  niver_motta.style.display="block";
+  body.style.backgroundColor="#FFD6E7";
   ff.textContent="feliz aniversario motta";
+  instagram.href="https://www.instagram.com/anallumotta_?igsi=amdtNnV4YW43Zzdx";
+
+  document.querySelector("#musica_motta").play();
+
+  tela_data.style.opacity="0";
+  tela_data.addEventListener("transitionend" , ()=>{
+    tela_data.style.display="none";} , {once:true});
+
+  niver_motta.style.display="block";
+  setTimeout(()=>{
+    niver_motta.style.opacity="1";}
+            ,50);
+  revelarAosPoucos();
+  
 }
 
-let data_chegada_motta= new Date("2026-05-01T00:00:00");
+function revelarAosPoucos() {
+  let elementos = document.querySelectorAll(".aparece");
+
+  elementos.forEach((elemento, index) => {
+    setTimeout(() => {
+      elemento.style.opacity = "1";
+    }, index * 500); // cada um espera 500ms a mais que o anterior
+  });
+  }
+
+let data_chegada_motta= new Date("2026-06-01T00:00:00");
 
 function atualizarRelogio() {
   let agora = new Date();
