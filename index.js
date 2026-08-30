76cbbdfd965f7304648f2e8c6ee6642eb2dada94
Anonimous157 <junior.filipi2009@gmail.com>
2026-08-30 02:01:02 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 let fate=document.querySelector("#data");
 let ff=document.querySelector(".hh");
 let tela_data=document.querySelector("#tela_data");
-let niver_motta=document.querySelector(".niver_motta");
+let niver_motta=document.querySelector("#niver_motta");
 let body=document.querySelector(".corpo");
-let textao=document.querySelector(".texto_principal")
 let instagram=document.querySelector(".instagram");
 let relogio=document.querySelector("#relogiom")
 let quem_tá_de_niver=0
@@ -13,7 +12,6 @@ let quem_tá_de_niver=0
 //=3 =>ARTHUR
 //=4 =>AMABILY
 //=5 =>LUCAS//
-const rosa="#ee92c7d8";
 
 fate.addEventListener("change", () => {
   if (fate.value==="2026-09-01"){
@@ -24,6 +22,7 @@ fate.addEventListener("change", () => {
     ff.textContent=fate.value;
   }
 })
+
 function motta() {
   body.style.backgroundColor="#ffd6e7df";
   ff.textContent="feliz aniversario motta";
@@ -40,33 +39,29 @@ function motta() {
     niver_motta.style.opacity="1";}
             ,50);
   revelarAosPoucos();
-  
 }
 
 function revelarAosPoucos() {
   let elementos = document.querySelectorAll(".aparece");
-
   elementos.forEach((elemento, index) => {
     setTimeout(() => {
       elemento.style.opacity = "1";
-    }, index * 500); // cada um espera 500ms a mais que o anterior
+    }, index * 500);
   });
-  }
+}
 
 let data_chegada_motta= new Date("2026-06-01T00:00:00");
 
 function atualizarRelogio() {
   let agora = new Date();
-  
-    let diferenca = agora - data_chegada_motta; // em milissegundos
+  let diferenca = agora - data_chegada_motta;
 
-    let dias = Math.floor(diferenca / 86400000);
-    let horas = Math.floor((diferenca % 86400000) / 3600000);
-    let minutos = Math.floor((diferenca % 3600000) / 60000);
-    let segundos = Math.floor((diferenca % 60000) / 1000);
+  let dias = Math.floor(diferenca / 86400000);
+  let horas = Math.floor((diferenca % 86400000) / 3600000);
+  let minutos = Math.floor((diferenca % 3600000) / 60000);
+  let segundos = Math.floor((diferenca % 60000) / 1000);
 
-    relogio.textContent=`~${dias} dias,${horas}h, ${minutos}m, ${segundos}s`;
+  relogio.textContent=`~${dias} dias,${horas}h, ${minutos}m, ${segundos}s`;
 }
 
 setInterval(atualizarRelogio, 1000);
-  
